8eb80e22b05bc0617614d016a621272ee5d3e538
Vladislav <chenskiy.vlad@mail.ru>
2023-04-05 19:46:41 +0300

    HM2

---
+++ HM2/HM2.sql
@@ -0,0 +1,66 @@
+CREATE DATABASE IF NOT EXISTS homework2;
+
+USE homework2;
+
+DROP TABLE IF EXISTS sales;
+
+CREATE TABLE IF NOT EXISTS sales
+(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    order_date DATE NOT NULL,
+    count_product INT NOT NULL
+);
+
+-- 1. Используя операторы языка SQL, создайте табличку “sales”. Заполните ее данными
+
+INSERT INTO sales (order_date, count_product)
+VALUES
+('2022-01-01', 156),
+('2022-01-02', 180),
+('2022-01-03', 21),
+('2022-01-04', 124),
+('2022-01-05', 341);
+
+-- SELECT * FROM sales;
+
+-- 2. Сгруппируйте значений количества в 3 сегмента — меньше 100, 100-300 и больше 300.
+
+/*
+SELECT 
+	id AS "id заказа",
+    IF (count_product < 100, 'Маленький заказ',
+		IF (count_product BETWEEN 100 AND 299, 'Средний заказ', 'Большой заказ'))
+	AS 'Тип заказа'
+FROM sales;
+*/
+
+-- 3. Создайте таблицу “orders”, заполните ее значениями. Покажите “полный” статус заказа, используя оператор CASE
+
+DROP TABLE IF EXISTS orders;
+
+CREATE TABLE IF NOT EXISTS orders
+(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    employee_id VARCHAR(40) NOT NULL,
+    amount FLOAT NOT NULL,
+    order_status VARCHAR(40) NOT NULL
+);
+
+INSERT INTO orders (employee_id, amount,order_status)
+VALUES
+('e03', 15.00, 'OPEN'),
+('e03', 15.00, 'OPEN'),
+('e03', 15.00, 'CLOSED'),
+('e03', 15.00, 'OPEN'),
+('e03', 15.00, 'CANCELLED');
+
+-- SELECT * FROM orders;
+
+SELECT id, employee_id, amount, order_status,
+CASE 
+	WHEN order_status = 'OPEN' THEN 'Order is in open state'
+    WHEN order_status = 'CLOSED' THEN 'Order is closed'
+    WHEN order_status = 'CANCELLED' THEN 'Order is cancelled'
+    ELSE 'ERROR 404'
+END AS full_order_status
+FROM orders;
